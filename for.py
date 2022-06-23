bea7be7b3661a3fd1270c8e009a714d63817acb0
cjh <rhtnxndbdb@gmail.com>
2022-06-23 15:54:14 +0900

    추천서비스 반복기능 추가

--- for.py
+++ for.py
@@ -1,2 +1,19 @@
-for i in range(3):
+import random
+for i in range(50):
     print(i)
+    print(f'{i+1}.hello world')
+    print('짜장면이냐 짬뽕이냐?')
+    a =print
+    print(f'니가 입력한 값: {a}')
+    menu = '짜장' , '짬뽕'
+    if a =='짜장' or a == '짬뽕':
+        print('나오나? 나오면 True') 
+    print('인공지능이 추천 해주는 결과는?')
+    m = random.choice(menu)
+    print(f"{m} 먹어!!")
+else:
+    print('짬짜면 먹어!!')
+    print('1 .hello world')
+    print('2 .hello world')
+    print('hello world')
+    print('hello world')
